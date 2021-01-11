2702338926e54a906cc86f7911bc60f7bd47f621
Watchful1 <watchful@watchful.gr>
2021-01-11 00:08:51 -0800

    Update post downloader

--- subDownloader.py
+++ subDownloader.py
@@ -3,25 +3,10 @@ from datetime import datetime
 import traceback
 import time
 import json
-import sys
 
-username = ""  # put the username you want to download in the quotes
-subreddit = ""  # put the subreddit you want to download in the quotes
-# leave either one blank to download an entire user's or subreddit's history
-# or fill in both to download a specific users history from a specific subreddit
+subreddit = "redditdev"
 
-filter_string = None
-if username == "" and subreddit == "":
-	print("Fill in either username or subreddit")
-	sys.exit(0)
-elif username == "" and subreddit != "":
-	filter_string = f"subreddit={subreddit}"
-elif username != "" and subreddit == "":
-	filter_string = f"author={username}"
-else:
-	filter_string = f"author={username}&subreddit={subreddit}"
-
-url = "https://api.pushshift.io/reddit/{}/search?limit=1000&sort=desc&{}&before="
+url = "https://api.pushshift.io/reddit/{}/search?limit=1000&sort=desc&subreddit={}&before="
 
 start_time = datetime.utcnow()
 
@@ -33,9 +18,9 @@ def downloadFromUrl(filename, object_type):
 	handle = open(filename, 'w')
 	previous_epoch = int(start_time.timestamp())
 	while True:
-		new_url = url.format(object_type, filter_string)+str(previous_epoch)
+		new_url = url.format(object_type, subreddit)+str(previous_epoch)
 		json_text = requests.get(new_url, headers={'User-Agent': "Post downloader by /u/Watchful1"})
-		time.sleep(1)  # pushshift has a rate limit, if we send requests too fast it will start returning error messages
+		time.sleep(1) # pushshift has a rate limit, if we send requests too fast it will start returning error messages
 		try:
 			json_data = json_text.json()
 		except json.decoder.JSONDecodeError:
@@ -57,7 +42,9 @@ def downloadFromUrl(filename, object_type):
 					handle.write(" : ")
 					handle.write(datetime.fromtimestamp(object['created_utc']).strftime("%Y-%m-%d"))
 					handle.write("\n")
-					handle.write(object['body'].encode(encoding='ascii', errors='ignore').decode())
+					text = object['body']
+					textASCII = text.encode(encoding='ascii', errors='ignore').decode()
+					handle.write(textASCII)
 					handle.write("\n-------------------------------\n")
 				except Exception as err:
 					print(f"Couldn't print comment: https://www.reddit.com{object['permalink']}")
@@ -67,11 +54,13 @@ def downloadFromUrl(filename, object_type):
 					if 'selftext' not in object:
 						continue
 					try:
-						handle.write(str(object['score']))
-						handle.write(" : ")
-						handle.write(datetime.fromtimestamp(object['created_utc']).strftime("%Y-%m-%d"))
-						handle.write("\n")
-						handle.write(object['selftext'].encode(encoding='ascii', errors='ignore').decode())
+						#handle.write(str(object['score']))
+						#handle.write(" : ")
+						#handle.write(datetime.fromtimestamp(object['created_utc']).strftime("%Y-%m-%d"))
+						#handle.write("\n")
+						text = object['selftext']
+						textASCII = text.encode(encoding='ascii', errors='ignore').decode()
+						handle.write(textASCII)
 						handle.write("\n-------------------------------\n")
 					except Exception as err:
 						print(f"Couldn't print post: {object['url']}")
@@ -84,4 +73,4 @@ def downloadFromUrl(filename, object_type):
 
 
 downloadFromUrl("posts.txt", "submission")
-downloadFromUrl("comments.txt", "comment")
+#downloadFromUrl("comments.txt", "comment")
